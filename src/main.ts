0c002d2cf4485e4da5e23b6b79dd8f2d01ca5d3d
Vladislav Zheromsky <vladislavzheromsky@gmail.com>
2024-11-24 19:49:09 +0100

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Application } from 'jsr:@oak/oak/application';
+import { oakCors } from 'https://deno.land/x/cors/mod.ts';
 import { Router } from '@oak/oak/router';
 import { apiRouter } from '@api/api-router.ts';
 import { errorHandler } from '@api/middleware/errors.ts';
@@ -10,6 +11,7 @@ if (import.meta.main) {
   router.use('/api', apiRouter.routes());
 
   app.use(errorHandler);
+  app.use(oakCors());
   app.use(router.routes());
   app.use(router.allowedMethods());
 
